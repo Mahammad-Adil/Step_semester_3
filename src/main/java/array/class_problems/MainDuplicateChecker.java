013f971bb8688c573078c1c974fe268ddfae0999
Mahammad Adil <mahammadadilshaik09@gmail.com>
2026-09-13 23:41:59 +0530

    Updated problem 3 assignment 4

---
+++ src/main/java/array/class_problems/MainDuplicateChecker.java
@@ -0,0 +1,49 @@
+```java
+import java.util.Scanner;
+
+class DuplicateChecker {
+
+    static boolean containsDuplicate(int[] nums) {
+
+        // Compare every pair
+        for (int i = 0; i < nums.length; i++) {
+
+            for (int j = i + 1; j < nums.length; j++) {
+
+                // If two different positions have same value
+                if (nums[i] == nums[j]) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
+
+
+public class MainDuplicateChecker {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter elements:");
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        boolean result = DuplicateChecker.containsDuplicate(nums);
+
+        System.out.println(result);
+
+        sc.close();
+    }
+}
+```
