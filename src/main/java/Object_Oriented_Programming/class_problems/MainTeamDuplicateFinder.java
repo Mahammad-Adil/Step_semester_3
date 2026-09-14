227964324876c878db7534f113f8a2c3e8790168
Mahammad Adil <mahammadadilshaik09@gmail.com>
2026-09-14 18:56:19 +0530

    2 out of 10

---
+++ src/main/java/Object_Oriented_Programming/class_problems/MainTeamDuplicateFinder.java
@@ -0,0 +1,35 @@
+```java
+class TeamDuplicateFinder {
+
+    static String findDuplicateTeam(String[] teamNames) {
+
+        for (int i = 0; i < teamNames.length; i++) {
+
+            for (int j = i + 1; j < teamNames.length; j++) {
+
+                if (teamNames[i].equals(teamNames[j])) {
+                    return "Duplicate Found: " + teamNames[i];
+                }
+            }
+        }
+
+        return "No Duplicates Found";
+    }
+}
+
+public class MainTeamDuplicateFinder {
+
+    public static void main(String[] args) {
+
+        String[] teamNames = {
+            "ByteForce",
+            "CodeCrafters",
+            "ByteForce"
+        };
+
+        String result = TeamDuplicateFinder.findDuplicateTeam(teamNames);
+
+        System.out.println(result);
+    }
+}
+```
