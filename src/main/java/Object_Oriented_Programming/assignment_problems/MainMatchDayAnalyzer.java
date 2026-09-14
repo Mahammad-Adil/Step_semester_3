b1b196dbd64b38f4c1aca722412b61f5640df2d6
Mahammad Adil <mahammadadilshaik09@gmail.com>
2026-09-14 20:03:32 +0530

    9 out of 10

---
+++ src/main/java/Object_Oriented_Programming/assignment_problems/MainMatchDayAnalyzer.java
@@ -0,0 +1,58 @@
+```java
+class MatchDayAnalyzer {
+
+    private static double rowAverage(int[] row) {
+
+        int sum = 0;
+
+        for (int runs : row) {
+            sum += runs;
+        }
+
+        return (double) sum / row.length;
+    }
+
+    static String classifyMatches(int[][] runsPerOver, int threshold) {
+
+        String result = "";
+
+        for (int i = 0; i < runsPerOver.length; i++) {
+
+            double average = rowAverage(runsPerOver[i]);
+
+            if (average >= threshold) {
+                result += "Match " + i + ": Power Surge";
+            } else {
+                result += "Match " + i + ": Normal";
+            }
+
+            if (i < runsPerOver.length - 1) {
+                result += " | ";
+            }
+        }
+
+        return result;
+    }
+}
+
+public class MainMatchDayAnalyzer {
+
+    public static void main(String[] args) {
+
+        int[][] runsPerOver = {
+            {4, 6, 8},
+            {10, 12, 14},
+            {2, 3, 1}
+        };
+
+        int threshold = 8;
+
+        String result =
+            MatchDayAnalyzer.classifyMatches(
+                runsPerOver, threshold
+            );
+
+        System.out.println(result);
+    }
+}
+```
